b315ba5db97d87797bc9e95f662f9c46ad0dde7b
BinaryJourney <71597536+BinaryJourney@users.noreply.github.com>
2022-10-26 11:17:25 +0200

    piechart beta

--- app/src/main/java/isep/fr/moneytracker/Fragments/PreviewFragment.java
+++ app/src/main/java/isep/fr/moneytracker/Fragments/PreviewFragment.java
@@ -1,13 +1,18 @@
 package isep.fr.moneytracker.Fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
 
 import isep.fr.moneytracker.R;
 import isep.fr.moneytracker.databinding.FragmentPreviewBinding;
@@ -16,15 +21,65 @@ public class PreviewFragment extends Fragment {
 
     private FragmentPreviewBinding binding;
 
+    TextView tvUltimatePurpose, tvPassion, tvPleasure, tvBoring, tvSadness, tvHell;
+    PieChart pieChart;
+
     @Override
-    public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState
-    ) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+//        tvUltimatePurpose = binding.tvUltimatePurpose;
+//        tvPassion = binding.tvPassion;
+//        tvPleasure = binding.tvPleasure;
+//        tvBoring = binding.tvBoring;
+//        tvSadness = binding.tvSadness;
+//        tvHell = binding.tvBoring;
+//        pieChart = binding.piechart;
+//        setData();
 
         binding = FragmentPreviewBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
+    }
+
+    private void setData() {
+        tvUltimatePurpose.setText(Integer.toString(5));
+        tvPassion.setText(Integer.toString(20));
+        tvPleasure.setText(Integer.toString(30));
+        tvBoring.setText(Integer.toString(20));
+        tvSadness.setText(Integer.toString(20));
+        tvHell.setText(Integer.toString(5));
+
+        pieChart.addPieSlice(
+                new PieModel(
+                        getResources().getString(R.string.pie_string_1),
+                        Integer.parseInt(tvUltimatePurpose.getText().toString()),
+                        getResources().getColor(R.color.red)));
+        pieChart.addPieSlice(
+                new PieModel(
+                        getResources().getString(R.string.pie_string_2),
+                        Integer.parseInt(tvPassion.getText().toString()),
+                        getResources().getColor(R.color.red_neutral_1)));
+        pieChart.addPieSlice(
+                new PieModel(
+                        getResources().getString(R.string.pie_string_3),
+                        Integer.parseInt(tvPleasure.getText().toString()),
+                        getResources().getColor(R.color.red_neutral_2)));
+        pieChart.addPieSlice(
+                new PieModel(
+                        getResources().getString(R.string.pie_string_4),
+                        Integer.parseInt(tvBoring.getText().toString()),
+                        getResources().getColor(R.color.red_neutral_3)));
+        pieChart.addPieSlice(
+                new PieModel(
+                        getResources().getString(R.string.pie_string_5),
+                        Integer.parseInt(tvSadness.getText().toString()),
+                        getResources().getColor(R.color.red_neutral_4)));
+        pieChart.addPieSlice(
+                new PieModel(
+                        getResources().getString(R.string.pie_string_6),
+                        Integer.parseInt(tvHell.getText().toString()),
+                        getResources().getColor(R.color.red_neutral_5)));
+        pieChart.startAnimation();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
--- app/src/main/java/isep/fr/moneytracker/MainActivity.java
+++ app/src/main/java/isep/fr/moneytracker/MainActivity.java
@@ -77,15 +77,6 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
